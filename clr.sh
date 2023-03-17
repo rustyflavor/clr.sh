6140db1b21e536a314d7c531317d4c2c3b3f2f2f
rustyflavor <rustyflavor@proton.me>
2023-03-16 19:39:08 -0700

    Initial upload of existing scripts

---
+++ clr.sh
@@ -0,0 +1,14 @@
+# Colors:  [r]ed [g]reen [b]lue [c]yan [m]agenta [y]ellow blac[k] [w]hite
+#          Capital letter for bright color, @ for terminal's default color.
+#          First color symbol is foreground, second is background.
+#          $(clr g):Green text, $(clr Gb):Bright Green on blue.
+#          To change background without affecting foreground, use a . for
+#          foreground color e.g. $(clr .g) leaves fg alone and sets bg green.
+# Formats: + Apply Bold,  _ Apply Underline
+#          = Remove Bold, - Remove Underline (same key as apply, but unshifted)
+#          Place format symbols before color symbols when combining them.
+clr()(local i=$1 l=krgybmcw@ b g c s
+n(){ x=${l%"${1,}"*};x=${#x};((x<8&&(x+=b+g)||(x=39+g)));printf %d $x;}
+while c=${i::1};[ "$c" ]&&((g<11));do i=${i:1};b=30;case $c in +)s+=";1";;
+=)s+=";22";;_)s+=";4";;-)s+=";24";;.)((g+=10));;[${l^^}])b=90;;&
+[${l^^}]|[$l])s+=";$(n "$c")";((g+=10));esac;done;printf %b "\e[${s#;}m")
---
+++ clr_demo.sh
@@ -0,0 +1,44 @@
+clr_demo () {
+  local example examples
+  examples=(
+'echo "$(clr _)Underline$(clr)"'
+'echo "$(clr +)Bold$(clr)"'
+'echo "$(clr +_)Bold Underline$(clr)"'
+'echo "$(clr +_g)Bold Underline Green$(clr)"'
+'echo "$(clr +_G)Bold Underline Bright Green$(clr)"'
+'echo "$(clr Gb)Bright Green on Dark Blue$(clr)"'
+'echo "$(clr gB)Dark Green on Bright Blue$(clr)"'
+'echo "$(clr G)Bright Green with $(clr +)two words$(clr =) in bold$(clr)"'
+'echo "$(clr cb)Cyan on Blue with $(clr G)a span$(clr c) of bright green text$(clr)"'
+'echo "$(clr cb)Cyan on Blue with $(clr .B)a span$(clr .b) of bright blue background$(clr)"'
+  )
+echo "Examples: "
+for example in "${examples[@]}"; do
+   printf "$ %s\n" "$example"
+   eval "$example"
+   echo
+done
+}
+clr_tables() {
+  local format desc bg fg code colors=( r R y Y g G b B c C m M k K w W . @)
+  for format in '' + _ _+; do
+    case $format in
+      '') desc="Normal";;
+      +)  desc="Bold";;
+      _)  desc="Underline";;
+      _+) desc="Bold Underline";;
+    esac
+    printf "\n%s:\n" "$desc"
+    for bg in "${colors[@]}"; do
+      [[ "$bg" = "." ]] && continue
+      for fg in "${colors[@]}"; do
+        code="${format}${fg}${bg}"
+        printf "%b %4s %b" "$(clr $code)" "$code" "$(clr)"
+      done
+      echo
+    done
+    [[ "$format" != "_+" ]] && { echo; read -p "More? [Y/n]: " reply; }
+    [[ -z "$reply" || "${reply,,}" == "y" ]] || return
+  done
+echo
+}

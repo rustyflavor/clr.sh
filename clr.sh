5ecbf9bb8acfd877c15e37235fb48dffc36b02db
rustyflavor <rustyflavor@proton.me>
2023-03-17 16:31:15 -0700

    Remove inner function, use substitution instead
    Turns out that inner function wasn't buying us anything, can just do its work
    within command substitution where we were calling the function in the final
    case clause. 19 bytes shorter and a little bit faster since we don't need a
    subshell anymore.

--- clr.sh
+++ clr.sh
@@ -8,9 +8,9 @@
 #          = Remove Bold, - Remove Underline (same key as apply, but unshifted)
 #          Place format symbols before color symbols when combining them.
 
-[ ${BASH_VERSINFO[0]} -ge 4 ]&&clr()(local i=$1 l=krgybmcw@ b g c s
-n(){ x=${l%"${1,}"*};x=${#x};((x<8&&(x+=b+g)||(x=39+g)));printf %d $x;}
+[ ${BASH_VERSINFO[0]} -ge 4 ]&&clr(){ local i=$1 l=krgybmcw@ b g c s
 while c=${i::1};[ "$c" ]&&((g<11));do i=${i:1};b=30;case $c in +)s+=";1";;
 =)s+=";22";;_)s+=";4";;-)s+=";24";;.)((g+=10));;[${l^^}])b=90;;&
-[${l^^}]|[$l])s+=";$(n "$c")";((g+=10));esac;done;printf %b "\e[${s#;}m")||
+[${l^^}]|[$l])s+=";$(x=${l%"${c,}"*};x=${#x};((x<8&&(x+=b+g)||(x=39+g)));
+printf %d $x)";((g+=10));esac;done;printf %b "\e[${s#;}m";}||
 clr(){ :;} # docs: https://github.com/rustyflavor/clr.sh

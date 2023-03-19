4d7c081c243b3b9870738d68d4a7bac45823c907
rustyflavor <rustyflavor@proton.me>
2023-03-19 09:51:58 -0700

    Use ternary operators and shorten cmd substitution
    Replace (( a && b || c)) with shorter (( a ? b : c)).
    Remove first printf and use simple assignment for color additions

--- clr.sh
+++ clr.sh
@@ -7,10 +7,8 @@
 # Formats: + Apply Bold,  _ Apply Underline
 #          = Remove Bold, - Remove Underline (same key as apply, but unshifted)
 #          Place format symbols before color symbols when combining them.
-
 [ ${BASH_VERSINFO[0]} -ge 4 ]&&clr(){ local i=$1 l=krgybmcw@ b g c s
 while c=${i::1};[ "$c" ]&&((g<11));do i=${i:1};b=30;case $c in +)s+=";1";;
-=)s+=";22";;_)s+=";4";;-)s+=";24";;.)((g+=10));;[${l^^}])b=90;;&
-[${l^^}]|[$l])s+=";$(x=${l%"${c,}"*};x=${#x};((x<8&&(x+=b+g)||(x=39+g)));
-printf %d $x)";((g+=10));esac;done;printf %b "\e[${s#;}m";}||
-clr(){ :;} # docs: https://github.com/rustyflavor/clr.sh
+=)s+=";22";;_)s+=";4";;-)s+=";24";;.)((g+=10));;[${l^^}])b=90;;& [${l^^}]|[$l])
+x=${l%"${c,}"*};x=${#x};x=$((x<8?x+b+g:39+g));s+=";$x";((g+=10));esac;done;
+printf %b "\e[${s#;}m";}||clr(){ :;} # https://github.com/rustyflavor/clr.sh
